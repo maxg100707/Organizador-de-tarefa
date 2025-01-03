7c347803614bbc928a579d3b8473d7b2f5431bbc
maxg100707 <maxguzman914@gmail.com>
2025-01-03 11:51:44 -0300

    Update script.js

--- script.js
+++ script.js
@@ -66,3 +66,26 @@ input.addEventListener("keyup", function (event) {
     btnAdd.click();
   }
 });
+
+/*
+        _             _
+       / \           / \
+      /   \         /   \
+     /     \_______/     \
+    /  ___           ___  \
+   |  / ()\         / ()\  |
+   |  \___/         \___/  |
+   |           ___         |
+   \          |___|        /
+    \           |         /
+     \      \___|___/    /
+      \_               _/
+        \_____________/  
+           MIAW MIAW
+
+
+instagram : https://www.instagram.com/maxg1007/
+portifolio : https://maxg100707.github.io/Portifolio/
+
+*/
+
